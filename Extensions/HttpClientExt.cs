8e9887bbc862a731a325655a1ecb34381256a5ab
Kamilla Nyborg Gregertsen <kamilla.gregertsen@soprasteria.com>
2026-01-13 12:53:40 +0100

    Refactor

--- Extensions/HttpClientExt.cs
+++ Extensions/HttpClientExt.cs
@@ -5,10 +5,6 @@ internal static class HttpClientExt
     public static async Task<JsonDocument> ReadJsonDocumentAsync(this HttpClient client, string requestUri)
     {
         using var response = await client.GetAsync(requestUri);
-        //throw new Exception(response.Content);
-        //response.EnsureSuccessStatusCode();
-        //return await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
         throw new Exception(await response.Content.ReadAsStringAsync());
-
     }
 }
